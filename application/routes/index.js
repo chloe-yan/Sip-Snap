41327213a5915c94adedbbc6a3c474e15090b1d6
chloe-yan <chloeyan365@gmail.com>
2022-04-13 19:33:59 -0700

    Update UI

--- application/routes/index.js
+++ application/routes/index.js
@@ -15,7 +15,7 @@ router.get('/register', (req, res, next) => {
 })
 
 router.get('/post', (req, res, next) => {
-  const questions = ["I am most grateful for . . .", "My favorite way to spend the day is . . .", "The words I’d like to live by are . . .", "I really wish others knew this about me . . .", "I feel happiest when . . .", "I couldn’t imagine living without . . .", "When I'm struggling, the kindest thing I can do for myself is . . .", "I feel most energized when . . .", "To my past self, . . ."];
+  const questions = ["I am most grateful for...", "My favorite way to spend the day is...", "The words I’d like to live by are...", "I really wish others knew this about me...", "I feel happiest when...", "I couldn’t imagine living without...", "When I'm struggling, the kindest thing I can do for myself is...", "I feel most energized when...", "To my past self,..."];
   var randomQ = questions[Math.floor(Math.random()*questions.length)]
   res.render('post', { title: 'Share your sip', question: randomQ});
 })
